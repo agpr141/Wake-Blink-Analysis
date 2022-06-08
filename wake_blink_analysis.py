886b35507402f5b6bfc72c51f467fdb6eb6f6e7b
agpr141 <47218099+agpr141@users.noreply.github.com>
2022-06-08 13:08:56 +0100

    Update wake_blink_analysis.py

--- wake_blink_analysis.py
+++ wake_blink_analysis.py
@@ -9,11 +9,13 @@ import os
 from blink_automatic_detection_funcs import *
 
 # 1. set path for 'Wake Eyes Open' .edf file & enter participant details
-pptid = '1102'
+pptid = '1119'
 night = '2'
 group = 'HC'
-os.chdir('Z:/Data/HC/1102/H1/EEG/Night 2/EDF/Eyes Open')  # change directory to folder of interest
-weo = 'N2weo.edf'  # define your .edf file
+os.chdir('Z:/Data/HC/1119/H1/EEG/Night 2/EDF/Eyes Open')  # change directory to folder of interest
+weo = '1119^weo.edf'  # define your .edf file
+if not os.path.exists('EOG-complete'):
+    os.makedirs('EOG-complete')
 
 # analyse
 eyeblink_stats = blink_analyse(weo, pptid, night, group)
